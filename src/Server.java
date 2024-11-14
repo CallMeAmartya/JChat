cf1222b7d15108c9c4254a25b259ff1adb74837a
Amartya Sen <amartyasen00077@gmail.com>
2024-11-14 16:14:26 +0530

    Adds main Server class

---
+++ src/Server.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+
+        try {
+            while(!this.serverSocket.isClosed()) {
+                Socket socket = this.serverSocket.accept();
+                System.out.println("A new client has connected: " + socket);
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServer();
+        }
+
+    }
+
+    private void closeServer() {
+        try {
+            if (this.serverSocket != null) {
+                this.serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+        ServerSocket ss = new ServerSocket(port);
+        Server server = new Server(ss);
+        server.startServer();
+    }
+}
